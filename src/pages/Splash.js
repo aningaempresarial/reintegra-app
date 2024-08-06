88e9eee628894a22bf78892cfbd7cc5f31986cfe
Sabrina Cristan <sabrinacristan@gmail.com>
2024-08-05 21:39:14 -0300

    ⏳ Adicionando tela splash

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { View, Image } from 'react-native';
 import styles from '../styles/Splash';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
+import { useEffect } from 'react';
 
 function Splash() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const temporizador = setTimeout(() => {
+      navigation.replace('Home');
+    }, 3000);
+    return () => clearTimeout(temporizador);
+  });
+
   return (
     <View style={styles.container}>
       <LinearGradient colors={['#ff5e43', '#ff8b44']} style={styles.background}/>

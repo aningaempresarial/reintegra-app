69ec5e25249566c6efdc87ab3d0da07b41540695
Sabrina Cristan <sabrina.cristan@gmail.com>
2024-08-22 23:41:38 -0300

    ⛵ Ajeitando navegação

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../pages/Home';
+import Start from '../pages/Start';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import Splash from '../pages/Splash';
+import Choice from '../pages/Choice';
 
 const Stack = createNativeStackNavigator();
 
 function Navigation() {
   return (
-    <Stack.Navigator initialRouteName='Splash'>
+    <Stack.Navigator initialRouteName='Start'>
       <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }}/>
-      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+      <Stack.Screen name="Start" component={Start} options={{ headerShown: false }}/>
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
       <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }}/>
+      <Stack.Screen name="Choice" component={Choice} options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
 }

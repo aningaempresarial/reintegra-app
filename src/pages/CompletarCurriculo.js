81647c4d280ec2797a7beaa7cb345d916028789f
anajuprogrammer06 <anajulima2006@gmail.com>
2024-10-10 17:25:50 -0300

    ttendo dar um espaço entre eles

--- src/pages/CompletarCurriculo.js
+++ src/pages/CompletarCurriculo.js
@@ -53,7 +53,7 @@ const CompletarCurriculo = () => {
                             <Input
                                 label={'Estado'}
                                 placeholder={'SP'}
-                                style={{ width: '20%' }}
+                                style={{ width: '30%' }}
                                 
                             />
                         </View>
--- src/styles/CompletarCurriculo.js
+++ src/styles/CompletarCurriculo.js
@@ -73,7 +73,7 @@ export default StyleSheet.create({
     },
     inputCidadeEstado: {
       flexDirection: 'row',
-      margin: 3,
+      margin: 5,
     },
     cardN: {
       height: 'auto',
